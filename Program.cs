39c31c9ad8e8aeda27b5d0f4b0f17cb2bfa11d1b
nazap31 <you@example.com>
2022-06-21 19:44:16 -0600

    menor, mayor o igual

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int num;
+Console.WriteLine("Porfavor ingrese un numero");
+num = int.Parse(Console.ReadLine());
+
+if (num == 5)
+{
+  Console.WriteLine("El numero ingresado es igual a 5");  
+}
+    else if (num > 5)
+    {
+      Console.WriteLine("El numero ingresado es mayor a 5");
+    }
+else if (num < 5)
+{
+  Console.WriteLine("El numero ingresado es menor a 5");
+}
+
+Console.WriteLine("Programa terminado!");
